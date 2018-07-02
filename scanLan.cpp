75bc0b8e3e65c90a9e815ad70eab13811964a448
WangWang <luckboystudent@gmail.com>
2018-07-02 06:33:00 +0000

    20180702

--- scanLan.cpp
+++ scanLan.cpp
@@ -12,33 +12,14 @@
 #include<net/ethernet.h>
 #include<ifaddrs.h>
 
-#define ARP 0x0806
-
 class infoHost{
 	private:
 		int send;
 		char src_mac[6];
-		char src_IPv4[4];
-		
+		char srcIP[4];
+		char srcIP6[16];
 
-	public : 
-		infoHost(char *interface){
-			if((send=socket(PF_PACKET,SOCK_RAW,htons(ETH_P_ALL)))<0){
-				perror("Failed to open socket");
-				exit(0);
-			}
-			getHostmac(&interface);
-			getHostip();
-
-		}
-		~infoHost(){
-		
-		}
-		infoHost(infoHost &host){
-		
-		}
-	
-		void getHostmac(char **interface){
+		void setHostmac(char **interface){
 			struct ifreq ifr;
 			memset(&ifr,0,sizeof(ifr));
 			memcpy(ifr.ifr_name,*interface,strlen(*interface));	
@@ -48,32 +29,63 @@ class infoHost{
 			}
 			memcpy(this->src_mac,ifr.ifr_hwaddr.sa_data,6);
 		}	
-		void getHostip(){
-			struct 	ifaddrs *ifaddr;
+		void setHostip( char *interface){
+			struct 	ifaddrs *ifaddr,*ifaptr;
 			getifaddrs(&ifaddr);
+			ifaptr=ifaddr;
 			while(ifaddr->ifa_next != NULL){
-				struct 	ifaddrs *ptr;
+				void *ptr;
 				ptr=ifaddr;
-				if(ifaddr->ifa_addr->sa_family == AF_INET){
-					void *ptr;
-					ptr=&((struct sockaddr_in *)ifaddr->ifa_addr)->sin_addr;
-					
-					printf("%s\n",ptr);
-					//ifaddr->ifa_addr->sin_addr;
-				}else if(ifaddr->ifa_addr->sa_family == AF_INET6){
+			
+				if(ifaddr->ifa_addr->sa_family == AF_INET && strcmp(ifaddr->ifa_name,interface)==0){
 				
-
+					ptr=&((struct sockaddr_in *)ifaddr->ifa_addr)->sin_addr;
+					memcpy(srcIP,ptr,4);
+				
+				}else if(ifaddr->ifa_addr->sa_family == AF_INET6 && strcmp(ifaddr->ifa_name,interface)==0){
+					ptr=&((struct sockaddr_in6 *)ifaddr->ifa_addr)->sin6_addr;
+					memcpy(srcIP6,ptr,16);
 				}
 				ifaddr=ifaddr->ifa_next;
-				freeifaddrs(ptr);
 			};
+			freeifaddrs(ifaptr);
 		}
+
+	public : 
+		infoHost(char *interface){
+			if((send=socket(PF_PACKET,SOCK_RAW,htons(ETH_P_ALL)))<0){
+				perror("Failed to open socket");
+				exit(0);
+			}
+			setHostmac(&interface);
+			setHostip(interface);
+
+		}
+		~infoHost(){
+			close(send);
+		}
+		infoHost(infoHost &host){
+		
+		}
+		char *getMac(){
+			return this->src_mac;
+		}
+		char *getIP(){
+			return this->srcIP;
+		}
+		char *getIP6(){
+			return this->srcIP6;
+		}
+	
 };
 
 
 int main(void ){
 	infoHost h("wlan0");
-//	h.show();
+	for(int i=0;i<strlen(h.getIP());i++){
+		printf("%x",h.getIP()[i]);
+	}
+
 
 	return 0;
 }
